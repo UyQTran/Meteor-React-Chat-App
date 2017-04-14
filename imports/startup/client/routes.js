982ea10115094366c8aece8291bdcd90cb329bb8
UyQTran <uqtontran@gmail.com>
2017-04-14 15:16:46 +0200

    Awaiting feedback

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,10 +1,2 @@
 import React from 'react';
 import AppLayout from '/imports/client/components/AppLayout.jsx';
-
-FlowRouter.notFound = {
-    action() {
-        mount(AppLayout, {
-                body: (<p>404 siden finnes ikke</p>)
-    });
-    }
-};
